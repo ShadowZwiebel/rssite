6af5531a186454ee97c183dbfcf540f0f2f58e7a
ShadowZwiebel <85753601+ShadowZwiebel@users.noreply.github.com>
2023-03-21 17:01:49 +0100

    config things

--- 3 try/tailwind.config.js
+++ 3 try/tailwind.config.js
@@ -1,7 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    theme: {
-      colors: {
-        'grey': '#'
-      },
+  content: [],
+  theme: {
+    colors: {
+      'grey': '#353535',
+      'logoRed': '#A8353A'
     },
-  }
+    extend: {},
+  },
+  plugins: [],
+}
